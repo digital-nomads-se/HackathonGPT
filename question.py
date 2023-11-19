8c89d3a1600fc87aadf6085f993820adef4b1ef1
Sagar Patel <sagarpatel@Sagars-MacBook-Pro.local>
2023-11-18 17:04:57 -0800

    Updated

--- question.py
+++ question.py
@@ -1,8 +1,8 @@
 prompt_template = '''
-<div class="chat-message user">
-    <div class="avatar">
+<div class="chat-message prompt">
+    <div class="prompt">
         <img src="https://pngimg.com/uploads/letter_q/letter_q_PNG11.png">
     </div>    
-    <div class="message">{{MSG}}</div>
+    <div class="response">{{MSG}}</div>
 </div>
 '''
