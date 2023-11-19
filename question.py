1c5259c7c1d7d34bed09b753468768377efac9ca
Sagar Patel <sagarpatel@Sagars-MacBook-Pro.local>
2023-11-18 16:42:05 -0800

    First Commit

---
+++ question.py
@@ -0,0 +1,8 @@
+prompt_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://pngimg.com/uploads/letter_q/letter_q_PNG11.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
